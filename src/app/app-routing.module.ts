02f913418ea53f3ee652e7f7075929c7ce8e3fc1
logeshkumaran-ui <logeshkumaranm@gmail.com>
2024-04-28 19:56:50 +0530

    app route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'weather', pathMatch: 'full' },
-  { path: 'weather', component: WeatherComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
